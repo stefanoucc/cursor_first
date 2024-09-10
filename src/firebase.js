55d8691aa163d029de7e79bd7e8aae26e753375b
stefanoucc <stefanou24@gmail.com>
2024-09-09 21:57:25 -0300

    asd

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: "  ",
   authDomain: "cursorfirst.firebaseapp.com",
   projectId: "cursorfirst",
   storageBucket: "cursorfirst.appspot.com",
